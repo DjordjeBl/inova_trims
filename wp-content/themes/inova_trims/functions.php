f6c9bc93f7ab4b99039cd5f9e275b9317c376c13
Djordje <djblagojevicpravni@gmail.com>
2016-09-21 01:09:44 +0200

    Enable Jquery

--- wp-content/themes/inova_trims/functions.php
+++ wp-content/themes/inova_trims/functions.php
@@ -104,9 +104,7 @@ add_action( 'widgets_init', 'inova_trims_widgets_init' );
 function inova_trims_scripts() {
 	wp_enqueue_style( 'inova_trims-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'inova_trims-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'inova_trims-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+    wp_enqueue_script( 'scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
