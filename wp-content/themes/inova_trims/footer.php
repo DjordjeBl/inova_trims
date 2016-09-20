711c8a8aba558c049535f104b604351e845ca440
Djordje <djblagojevicpravni@gmail.com>
2016-09-21 01:16:49 +0200

    Change footer and header

--- wp-content/themes/inova_trims/footer.php
+++ wp-content/themes/inova_trims/footer.php
@@ -15,9 +15,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'inova_trims' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'inova_trims' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'inova_trims' ), 'inova_trims', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+		
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
