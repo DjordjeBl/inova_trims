6e407356464535d017c79d4ccdecc36295183ab5
Djordje <djblagojevicpravni@gmail.com>
2016-09-21 04:38:00 +0200

    Add footer, add font-family

--- wp-content/themes/inova_trims/footer.php
+++ wp-content/themes/inova_trims/footer.php
@@ -15,7 +15,15 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-		
+			<div class="footer-info">
+				<h2>Sviđaju vam se naši proizvodi?</h2>
+					<ul class="footer-menu">
+						<li>Telefon: 062/651-749</li>
+						<li>E-mail: trims@mts.rs</li>
+						<li>Adresa: Smederevo, Milentija Popovića br. 13</li>
+					</ul>
+			</div>
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
