d6f365d98427cce619b76bfd172a13c6a7ae90d0
Djordje <djblagojevicpravni@gmail.com>
2016-09-28 01:10:56 +0200

    change in gallery-template col-xs-12

--- wp-content/themes/inova_trims/gallery-template.php
+++ wp-content/themes/inova_trims/gallery-template.php
@@ -6,7 +6,7 @@
 				<?php if ($images): ?>
 					<div class="gallery-1">
 						<?php foreach ($images as $image): ?>
-							<a href="<?php echo $image['url']; ?>" data-lightbox="roadtrip" class="col-xs-6 col-sm-6 col-lg-4">
+							<a href="<?php echo $image['url']; ?>" data-lightbox="roadtrip" class="col-xs-12 col-sm-6 col-lg-4">
 								<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
 							</a>
 						<?php endforeach; ?>
