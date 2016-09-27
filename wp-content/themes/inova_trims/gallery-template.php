fb62cf48596fc0b6cdbf1ae63badc993fffebb49
Djordje <djblagojevicpravni@gmail.com>
2016-09-28 00:38:49 +0200

    setup acf

--- wp-content/themes/inova_trims/gallery-template.php
+++ wp-content/themes/inova_trims/gallery-template.php
@@ -2,4 +2,15 @@
 
 <?php get_header(); ?>
 
+<?php $images = get_field('gallery'); ?>
+				<?php if ($images): ?>
+					<div class="gallery">
+						<?php foreach ($images as $image): ?>
+							<a href="<?php echo $image['url']; ?>">
+								<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+							</a>
+						<?php endforeach; ?>
+					</div>
+				<?php endif; ?>
+
 <?php get_footer(); ?>
