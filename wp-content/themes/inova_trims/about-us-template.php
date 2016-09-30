8ec214237c7b84c303f94ed6a6ff2519ffbd6c58
Djordje <djblagojevicpravni@gmail.com>
2016-09-30 14:43:49 +0200

    Change some paragraphs in about us page

--- wp-content/themes/inova_trims/about-us-template.php
+++ wp-content/themes/inova_trims/about-us-template.php
@@ -1,21 +1,23 @@
 <?php /* Template Name: About us */ ?>
 
 <?php get_header(); ?>
+
 <h1>O nama</h1>
 <hr>
 <!-- <div class="dragan-image">
     <img src="http://localhost/inova_trims/wp-content/uploads/2016/09/dragan.jpg">
 </div>   -->
 <div class="page-content">
-<p>Kompanija je prvobitno osnovana još 1991 godine, pod nazivom TRIMS, sa osnovnom idejom da radi na daljem razvoju uređaja za pozicioniranje železničkih vagona duž železničkih koloseka.Uređaj je bio zasnovan na patentiranom tehničkom rešenju.</p>
+
+<p>Inova Trims se bavi izradom kvalitetnih zidnih kreveta različitih veličina i modela. Kompanija je osnovana 1991. godine, pod nazivom TRIMS, sa idejom da radi na razvoju uređaja za pozicioniranje železničkih vagona duž železničkih koloseka.Uređaj je bio zasnovan na patentiranom tehničkom rešenju.<p>
 
 <p>Prva poslovna primena ovakvog rešanja je bila ostvarena 1994. godine na čeličani u Smederevu za prevlačenje lonca sa belim sirovim gvožđem na deonici od miksera do konvertora. Uređaj je pokazao odlične rezultate i prevukao je više miliona tona belog sirovog gvožđa.</p>
 
-<p>Međutim, događaji koji su zahvatili čitav naš region, tih godina su imali izuzetno nepovoljan uticaj na razvoj ovog projekta i posle niza godina smo odlučili da ugasimo tadašnju kompaniju.</p>
+<p>Događaji koji su zahvatili čitav naš region imali su nepovoljan uticaj na razvoj ovog projekta. Nakon nekoliko godina smo odlučili da ugasimo tadašnju kompaniju.</p>
 
-<p>U međuvremenu je naš tim neumorno nastavio da radi na novim patentima i tehničkim rešenjima u različitim oblastima. Na kraju,ove 2016. godine, ponovo smo otvorili kompaniju pod nazivom <strong>INOVA TRIMS</strong> koja je izašla na tržište sa nekolkiko originalnih proizvoda koji su našli svoje mesto na tržištu. To su između ostalog uređaji za <strong>zidne krevete</strong> koji omogućavaju izradu zidnih kreveta svih vrsta i veličina.</p>
+<p>U međuvremenu je naš tim neumorno nastavio da radi na novim patentima i tehničkim rešenjima u različitim oblastima. Ove 2016. godine, ponovo smo otvorili kompaniju pod nazivom INOVA TRIMS koja je izašla na tržište sa originalnim proizvodima. To su između ostalog uređaji za zidne krevete koji omogućavaju izradu zidnih kreveta svih vrsta i veličina. Naši uređaji omogućuju izradu zidnih kreveta koji se veoma lako zatvaraju i otvaraju, bukvalno sa dva prsta.</p>
 
-<p>Naše proizvode plasiramo u skoro svim zemljama u okruženju i možemo da realizujemo svaku vašu ideju i zamisao, da ispunimo svaki vaš zahtev u pogledu oblika, dizajna i veličine <strong>zidnih kreveta.</strong></P>
+<p>Naše proizvode plasiramo u skoro svim zemljama u okruženju. Možemo da realizujemo svaku vašu ideju i zamisao, da ispunimo svaki vaš zahtev u pogledu oblika, dizajna i veličine zidnih kreveta.</p>
 </div>
     
 
