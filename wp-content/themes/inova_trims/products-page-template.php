632608500e27c72e4358d9bcdd43a7aa735f0102
Djordje <djblagojevicpravni@gmail.com>
2016-10-10 22:04:56 +0200

    Fix php code for products page

--- wp-content/themes/inova_trims/products-page-template.php
+++ wp-content/themes/inova_trims/products-page-template.php
@@ -4,29 +4,25 @@
 
 <div class="page-content">
     <?php 
-    $args = array(
-        'numberposts' => -1,
-        'post_type' => 'proizvodi',
-        'orderby' => 'title',
-        'order' =>'ASC',
-    );
-    
-    $myposts = get_posts($args);
-    ?>
-    <div class="row">
-  <?php if($myposts):
-        foreach ($myposts as $mypost): ?>
-    
-        <div class="col-xs-12">
+$args = array(
+    'numberposts' => -1,
+    'post_type' => 'proizvodi',
+    'orderby' => 'title',
+    'order' =>'ASC',
+);
+
+$loop = new WP_Query( $args );
+?>
+<div class="row">
+    <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+        <div class="col-xs-12 col-md-6 col-lg-4">
             <div><?php the_field('oznaka'); ?></div>
             <div><?php the_field('velicina_duseka'); ?></div>
-            <div><?php the_field('oblik_kreveta'); ?></div>
+            <img src="<?php the_field('oblik_kreveta'); ?>"/>
             <div><?php the_field('cena_mehanizama'); ?></div>
         </div>
-                    
-        <?php endforeach; wp_reset_postdata(); ?>
-    <?php endif; ?>
-    </div>
+    <?php endwhile; wp_reset_postdata(); ?>
+</div>
 </div>
 
-<?php get_footer(); ?>
+<?php get_footer(); ?
