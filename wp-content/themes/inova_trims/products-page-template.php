eeb0e7c9bf3f8832f1c1bcafc321fa8626c841ce
Djordje <djblagojevicpravni@gmail.com>
2016-10-11 01:18:09 +0200

    Change styles in products page

--- wp-content/themes/inova_trims/products-page-template.php
+++ wp-content/themes/inova_trims/products-page-template.php
@@ -5,7 +5,7 @@
 <div class="page-content">
     <?php 
 $args = array(
-    'numberposts' => -1,
+    'posts_per_page' => -1,
     'post_type' => 'proizvodi',
     'order' =>'ASC',
 );
@@ -15,10 +15,15 @@ $loop = new WP_Query( $args );
 <div class="row">
     <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
         <div class="col-xs-12 col-md-6 col-lg-4">
-            <div><?php the_field('oznaka'); ?></div>
-            <div><?php the_field('velicina_duseka'); ?></div>
-            <img src="<?php echo get_field('oblik_kreveta')['url']; ?>"/>
-            <div><?php the_field('cena_mehanizama'); ?></div>
+            <ul class="product-list">
+                <li>
+                     <div><?php the_field('oznaka'); ?></div>
+                     <div>Veličina dušeka:</div>  
+                     <div><?php the_field('velicina_duseka'); ?></div>
+                     <img src="<?php echo get_field('oblik_kreveta')['url']; ?>"/>
+                     <div><?php the_field('cena_mehanizama'); ?></div>
+                </li>
+            </ul>
         </div>
     <?php endwhile; wp_reset_postdata(); ?>
 </div>
